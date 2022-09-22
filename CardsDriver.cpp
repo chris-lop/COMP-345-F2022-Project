ec49278478ed68d73c43fb7c18ce9bcf653a7c64
Marc Scattolin <marcscattolin@gmail.com>
2022-09-22 13:08:37 -0400

    fix include and file list in tasks.json

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "Cards.h"
 
 using std::cout, std::endl, std::string;
 
 void testCards() {
     cout << "In testcards" << endl;
+    Card card("diplomacy");
+    card.play();
 }
