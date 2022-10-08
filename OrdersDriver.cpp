f92f745febf0b61383412b4f74bb34f13ad8701d
Marita Brichan <maritabrichan@gmail.com>
2022-10-08 02:45:41 -0400

    Fixed memory leaks for orders

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -105,5 +105,7 @@ void testOrdersLists(){
         std::cout << *list->getOrder().at(i) << ' ';
     }
 
+    delete order, order2, deploy2, deploy, advance, bomb, blockade, airlift, negotiate;
+
 }
 
