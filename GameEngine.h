d440f41977faf91a4fd0add7467200590f1acce8
miraaji <97921445+miraaji@users.noreply.github.com>
2022-10-09 16:44:40 -0400

    Update GameEngine.h

--- GameEngine.h
+++ GameEngine.h
@@ -3,34 +3,26 @@
 #include<string>
 #include <iostream>
 #include "Player.h"
+#pragma once
+#include <string>
 
-void testGameStates();
-
-class GameEngine
-{
-   
-    private:
-    int number;
+class GameEngine {
+private:
+    int state;
 public:
-int getNumber();
-void setNumber(int num);
-//constructor and destructor
-GameEngine();
-~GameEngine();
-GameEngine(int number);
-  //copy constructor
-    GameEngine(const GameEngine& g1);
-
-    GameEngine& operator=(const GameEngine& ga);
-       //stream insertion operator
-    friend std::ostream& operator << (std::ostream& strm, const GameEngine& g);
+    GameEngine();
+   ~GameEngine();
+   GameEngine& operator=(const GameEngine& ga);
+   friend std::ostream& operator << (std::ostream& strm, const GameEngine& g);
     friend std::istream& operator >> (std::istream& in, GameEngine& g);
-
-  void start();
-   void loaded();
-   void validated();
-   void assign();
-   void issue();
-   void execute();
-   void win();
+    void handleInput(std::string line);
+    void loadMap();
+    void validateMap();
+    void addPlayers();
+    void assignReinforcement();
+    void issueOrders();
+    void executeOrders();
+    void win();
+    void startMessage();
+    bool finished();
 };
