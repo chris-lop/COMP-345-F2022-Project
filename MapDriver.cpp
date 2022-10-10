2a1b25d79d72b9c1c4f78b48db726fa1868ce496
lealakkis <lealakkis@outlook.com>
2022-10-09 20:06:19 -0400

    added other path

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -7,8 +7,14 @@ void testLoadMaps()
     // test read of file
     Map *m = new Map();
     MapLoader *ml = new MapLoader();
+    Map *m1 = new Map();
+    MapLoader *ml1 = new MapLoader();
+    Map *m2 = new Map();
+    MapLoader *ml2 = new MapLoader();
 
     auto graph = ml->loadMap("./3D.map");
+    auto graph1 = ml1->loadMap("./Mongol Empire 1294.map");
+    auto graph2 = ml2->loadMap("./_22_ PAC-MAN.map");
     
     // CHECK for map validity
     if (graph->territories.size() == 0) {
@@ -25,9 +31,17 @@ void testLoadMaps()
     // 2. continents are connected subgraphs
     // 3. each country belongs to one and only one continent
     m->validate(graph->territories);
+    m->validate(graph1->territories);
+    m->validate(graph2->territories);
 
     // Calling Destructors
     delete m;
     delete ml;
+    delete m1;
+    delete m2;
+    delete ml1;
+    delete ml2;
     delete graph;
+    delete graph1;
+    delete graph2;
 }
