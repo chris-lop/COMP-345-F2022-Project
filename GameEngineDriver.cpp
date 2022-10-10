1690b6458824b0510591d9378e5fa99bc45def18
miraaji <97921445+miraaji@users.noreply.github.com>
2022-10-09 20:51:45 -0400

    Update GameEngineDriver.cpp

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -1,17 +1,16 @@
 
 #include <iostream>
-
 #include "GameEngine.h"
 #include <string>
 using std::cin; using std::cout; using std::string; using std::endl;
 
 void testGameStates() {
-    GameEngine *engine = new GameEngine();
-    engine->startMessage();
-    while (!engine->finished()) {
+    GameEngine *engine = new GameEngine(); //creates a pointer to a GameEngine object
+    engine->startMessage(); //prints out the start message
+    while (!engine->finished()) { //keeps on taking user input as long as the final state has not been reached
         string line;
         cin>>line;
-        engine->handleInput(line);
+        engine->handleInput(line); //passes input to the handleInput(string) method to pass through the phases depending on the user's input
     }
    
 }
