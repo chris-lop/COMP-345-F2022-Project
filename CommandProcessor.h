62a01bbeb9e83d0181a559da763fd364c869886d
miraaji <97921445+miraaji@users.noreply.github.com>
2022-11-07 13:56:41 -0500

    Create CommandProcessor.h

---
+++ CommandProcessor.h
@@ -0,0 +1,22 @@
+
+#include "Command.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class CommandProcessor{
+private:
+    Command* c;
+    string state;
+    void readCommand();
+    bool done;
+
+public:
+void start();
+void startMessage();
+    void playegame(string line);
+    bool validate(string command);
+    void saveCommand(string command);
+    CommandProcessor();
+    ~CommandProcessor();
+};
