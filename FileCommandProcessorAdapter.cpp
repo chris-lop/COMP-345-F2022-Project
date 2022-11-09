1d8c4d248588e37fd058fb9917ab73b053e4c24f
miraaji <97921445+miraaji@users.noreply.github.com>
2022-11-08 21:46:00 -0500

    Update FileCommandProcessorAdapter.cpp

--- FileCommandProcessorAdapter.cpp
+++ FileCommandProcessorAdapter.cpp
@@ -26,12 +26,18 @@ void FileCommandProcessorAdapter::set_f_name(string name){
 
 //overriding the readCommand() method from the base class
 void FileCommandProcessorAdapter:: readCommand(){
-     bool valid;
+     string ycommand;
    //read the commands from a file
    vector<string*> commands=f->readLineFromFile(f_name);
    for(int i=0;i<commands.size();i++){
+       //saves the command
        saveCommand(*(commands.at(i)));
-       valid=validate(*(commands.at(i)));
+       //validate will save effect of invalid commands and return a string of the effect whether valid or invalid
+       ycommand=validate(*(commands.at(i)));
+       //saves the effect of the command if the command is valid
+       if(getvalid()==true){
+      get_c()->saveEffect(ycommand);
+       }
        playegame(*(commands.at(i)));
    }
     
