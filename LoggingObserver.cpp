000b1afd7659e13d971d99bdece5edfb443b2e12
Marita Brichan <maritabarikhan@hotmail.com>
2022-11-09 22:38:05 -0500

    Fixed a file mixup

--- LoggingObserver.cpp
+++ LoggingObserver.cpp
@@ -1,9 +1,96 @@
 #include <iostream> 
-#include "LoggingObserver.h"
 #include <algorithm>
+#include <fstream>
+#include "LoggingObserver.h"
 
-using std::cout;
+using std::cout; 
+using std::string; 
+using std::list;
 using std::endl;
+using std::ofstream;
 
-void testOrdersLists(){
+
+//Observer class
+Observer::Observer(){
+}
+
+Observer::~Observer(){
+}
+
+Observer::Observer(const Observer& o1){
+}
+
+Observer& Observer::operator=(const Observer& o1){
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream &strm, const Observer &o){
+    return strm;
+}
+
+
+//class LogObserver
+LogObserver::LogObserver(){
+}
+
+LogObserver::~LogObserver(){
+}
+
+LogObserver::LogObserver(const LogObserver& lo){
+}
+
+LogObserver& LogObserver::operator=(const LogObserver& lo){
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream &strm, const LogObserver &lo){
+    return strm;
+}
+
+void LogObserver::update(ILoggable* il){
+    //write into log file
+    ofstream logFile("gamelog.txt");
+    logFile << il->stringToLog() << endl;
+    logFile.close();
+}
+
+//Class Subject
+Subject::Subject(){
+   this->_observers = new list<Observer*>;
+}
+
+Subject::~Subject(){ 
+    delete this->_observers;
+}
+
+void Subject::attach(Observer* o){
+    this->_observers->push_back(o);
+}
+
+void Subject::detach(Observer* o){
+    this->_observers->remove(o);
+}
+
+void Subject::notify(ILoggable* il){
+    list<Observer *>::iterator i = _observers->begin();
+    for (; i != _observers->end(); ++i)
+    (*i)->update(il);
+}
+
+//Class ILoggable
+ILoggable::ILoggable(){
+}
+
+ILoggable::~ILoggable(){
+}
+
+ILoggable::ILoggable(const ILoggable& il){
+}
+
+ILoggable& ILoggable::operator=(const ILoggable& il){
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream &strm, const ILoggable &il){
+    return strm;
 }
--- LoggingObserverDriver.cpp
+++ LoggingObserverDriver.cpp
@@ -1,99 +1,13 @@
 #include <iostream> 
-#include <algorithm>
-#include <fstream>
 #include "LoggingObserver.h"
+#include <algorithm>
 
-using std::cout; 
-using std::string; 
-using std::list;
+using std::cout;
 using std::endl;
-using std::ofstream;
 
-
-//Observer class
-Observer::Observer(){
+void testOrdersLists(){
 }
 
-Observer::~Observer(){
-}
-
-Observer::Observer(const Observer& o1){
-}
-
-Observer& Observer::operator=(const Observer& o1){
-    return *this;
-}
-
-std::ostream& operator<<(std::ostream &strm, const Observer &o){
-    return strm;
-}
-
-
-//class LogObserver
-LogObserver::LogObserver(){
-}
-
-LogObserver::~LogObserver(){
-}
-
-LogObserver::LogObserver(const LogObserver& lo){
-}
-
-LogObserver& LogObserver::operator=(const LogObserver& lo){
-    return *this;
-}
-
-std::ostream& operator<<(std::ostream &strm, const LogObserver &lo){
-    return strm;
-}
-
-void LogObserver::update(ILoggable* il){
-    //write into log file
-    ofstream logFile("gamelog.txt");
-    logFile << il->stringToLog() << endl;
-    logFile.close();
-}
-
-//Class Subject
-Subject::Subject(){
-   this->_observers = new list<Observer*>;
-}
-
-Subject::~Subject(){ 
-    delete this->_observers;
-}
-
-void Subject::attach(Observer* o){
-    this->_observers->push_back(o);
-}
-
-void Subject::detach(Observer* o){
-    this->_observers->remove(o);
-}
-
-void Subject::notify(ILoggable* il){
-    list<Observer *>::iterator i = _observers->begin();
-    for (; i != _observers->end(); ++i)
-    (*i)->update(il);
-}
-
-//Class ILoggable
-ILoggable::ILoggable(){
-}
-
-ILoggable::~ILoggable(){
-}
-
-ILoggable::ILoggable(const ILoggable& il){
-}
-
-ILoggable& ILoggable::operator=(const ILoggable& il){
-    return *this;
-}
-
-std::ostream& operator<<(std::ostream &strm, const ILoggable &il){
-    return strm;
-}
 
 
 
