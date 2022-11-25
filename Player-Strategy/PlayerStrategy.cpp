c6b69cea8b83595a9f34007a3296515c5aa969e1
Chris Lopez <73512428+chris-lop@users.noreply.github.com>
2022-11-25 12:45:18 -0500

    Restored Original toAttack and toDefend methods for Aggressive Strategy

--- Player-Strategy/PlayerStrategy.cpp
+++ Player-Strategy/PlayerStrategy.cpp
@@ -16,23 +16,17 @@ PlayerStrategy::~PlayerStrategy() {
 AggressivePlayerStrategy::AggressivePlayerStrategy(Player* p): PlayerStrategy(p) {}
 AggressivePlayerStrategy::~AggressivePlayerStrategy() {}
 
-// issueOrder (How does an aggressive player decide which order to issue)
-void AggressivePlayerStrategy::issueOrder()
-{
-    
-}
-
-// toAttack (How does an aggressive player decide which territories to attack)
+// toAttack (list of territories to attack)
 std::vector <Territory*> AggressivePlayerStrategy::toAttack()
 {
-    vector <Territory*> result_attack;
-    vector<Territory*> trt = this->p->get_trt();
-
-    for (Territory* t : trt){
+    //vector territory to be returned as result
+    std::vector <Territory*> result_attack;
+ 
+    for (Territory* t : this->trt){
         //traverse the list of adjacent territories for each territory t
         for(Territory* t_adj: t->getAdjTerritories()){
 
-            if ((find(trt.begin(), trt.end(), t_adj)) != trt.end()){
+            if ((std::find(trt.begin(), trt.end(), t_adj)) != trt.end()){
                 //The adjacent territory was found in the list of owned territories
                 //We don't need to attack the territory t
                 continue;
@@ -41,7 +35,7 @@ std::vector <Territory*> AggressivePlayerStrategy::toAttack()
             //The adjacent territory was not found in owned territories, thus, the territory t_adj can be attacked
             else{
                 //if the territory to attack was already added, skip
-                if((find(result_attack.begin(),result_attack.end(), t_adj)) != result_attack.end()){
+                if((std::find(result_attack.begin(),result_attack.end(), t_adj)) != result_attack.end()){
                     continue;
                 }
                 //if not, add to the result vector
@@ -54,10 +48,42 @@ std::vector <Territory*> AggressivePlayerStrategy::toAttack()
     return result_attack;
 }
 
-// toDefend (How does an aggressive player decide which territories to defend)
+// toDefend (list of territories to defend)
 std::vector <Territory*> AggressivePlayerStrategy::toDefend()
 {
-    return vector<Territory*>{};
+    //new vector of Territory to be returned as result
+    std::vector <Territory*> result_defend; 
+
+    for (Territory* t : this->trt){
+        
+        //traverse the list of adjacent territories for each t 
+        for(Territory* t_adj: t->getAdjTerritories()){
+            //verify if each territory in the adjacent territories list is in the list of territories owned
+            
+            if ((std::find(trt.begin(), trt.end(), t_adj)) != trt.end()){
+                //The adjacent territory was found in the list of owned territories
+                //We don't need to defend the territory t
+                continue;
+            }
+
+            //The adjacent territory was not found, thus, the territory t must be defended
+            else{
+                //if the territory to defend was already added, skip
+                if((std::find(result_defend.begin(),result_defend.end(), t)) != result_defend.end()){
+                    continue;
+                }
+                //if not, add to the result vector
+                else{result_defend.push_back(t);}
+            }
+        }
+    }
+    return result_defend; 
+}
+
+// issueOrder (How does an aggressive player decide which order to issue)
+void AggressivePlayerStrategy::issueOrder()
+{
+    
 }
 
 //-----------------------------------//
@@ -69,10 +95,8 @@ NeutralPlayerStrategy::NeutralPlayerStrategy(Player* p): PlayerStrategy(p) {}
 NeutralPlayerStrategy::~NeutralPlayerStrategy() {}
 
 // issueOrder (How does a neutral player decide which order to issue)
-void NeutralPlayerStrategy::issueOrder()
-{
-    
-}
+// Neutral players never issue any orders or issue any cards, therefore they never will issueOrder()
+void NeutralPlayerStrategy::issueOrder() { }
 
 // toAttack (How does a neutral player decide which territories to attack)
 // Neutral players never issue any orders or issue any cards, therefore they never will never attack
